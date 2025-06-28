26f6b31e61a29958ae7238156216df955ed461c4
TahaIr88 <taha.iravani1@gmail.com>
2025-06-28 19:11:02 +0330

    add mudals to app.py

--- challenges/api/app.py
+++ challenges/api/app.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
+from flask import Flask, request, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+import logging
+from logging.handlers import RotatingFileHandler
+import re
 
-app = FastAPI()
-
-@app.get("/")
-async def hello():
-    return {"message": "Hello World!"}
